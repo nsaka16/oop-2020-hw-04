b370862627b21017f84e728965ee4577f7720359
unknown <nsaka16@freeuni.edu.ge>
2020-06-10 23:59:47 +0400

    Web loader partlly done, Couldnt complete

--- WebFrame.java
+++ WebFrame.java
@@ -2,27 +2,45 @@ import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
+//I needed help for this part of homework
+//Couldn't complete it. :(
 public class WebFrame {
     private JFrame frame;
     private JPanel panel;
     private JTable table;
     private JTextField numThreadField;
-    private JLabel runningLabel;
-    private JLabel completeLabel;
-    private JLabel elapsedLabel;
+    private JLabel labelOfCurrentlyRunning;
+    private JLabel labelOfAlreadyFinished;
+    private JLabel labelOfElapsedTime;
     private JProgressBar progressBar;
     private DefaultTableModel defaultTableModel;
-    private Launcher launcher;
+    private Thread launcherThread   ;
     private int activeThreads;
     private int finishedThreads;
-    private File file;
-    private Object object = new Object();   //Locking on inner created object, instead of root class instance.
+    private Lock lock = new ReentrantLock();
+    //Which one to use?
+    private AtomicInteger atomicInteger = new AtomicInteger();
 
-    public WebFrame(String fileName){
-        file = new File(fileName);
+    public WebFrame(String fileName) throws FileNotFoundException {
         initialiseSwingComponents();
+        readFile(fileName);
+    }
+
+    private void readFile(String fileName) throws FileNotFoundException {
+        File file = new File(fileName);
+        Scanner scanner = new Scanner(new FileReader(file));
+        while (scanner.hasNext()) {
+            defaultTableModel.addRow(new String[]{scanner.next(), ""});
+        }
+        scanner.close();
     }
 
     private void initialiseSwingComponents() {
@@ -34,21 +52,21 @@ public class WebFrame {
     }
 
     private void initialiseProgressBar() {
-        progressBar = new JProgressBar(0,defaultTableModel.getRowCount());
+        progressBar = new JProgressBar(0, defaultTableModel.getRowCount());
         panel.add(progressBar);
     }
 
     private void initialiseLabels() {
-        runningLabel = new JLabel("Running:0");
-        completeLabel = new JLabel("Completed:0");
-        elapsedLabel = new JLabel("Elapsed:");
-        panel.add(runningLabel);
-        panel.add(completeLabel);
-        panel.add(elapsedLabel);
+        labelOfCurrentlyRunning = new JLabel("Running:0");
+        labelOfAlreadyFinished = new JLabel("Completed:0");
+        labelOfElapsedTime = new JLabel("Elapsed:");
+        panel.add(labelOfCurrentlyRunning);
+        panel.add(labelOfAlreadyFinished);
+        panel.add(labelOfElapsedTime);
     }
 
     private void initialiseButtons() {
-        panel.add(Box.createRigidArea(new Dimension(0,5)));
+        panel.add(Box.createRigidArea(new Dimension(0, 5)));
         initialiseSingleThreadButton();
         initialiseSeveralThreadButton();
         initialiseStopButton();
@@ -57,15 +75,15 @@ public class WebFrame {
     private void initialiseStopButton() {
         JButton stopButton = new JButton("Stop");
         panel.add(stopButton);
-        stopButton.addActionListener(e->{
-            launcher.interrupt();
+        stopButton.addActionListener(e -> {
+            launcherThread.interrupt();
         });
     }
 
     private void initialiseSeveralThreadButton() {
         JButton concurrentFetch = new JButton("Concurrent Fetch");
         panel.add(concurrentFetch);
-        concurrentFetch.addActionListener(e->{
+        concurrentFetch.addActionListener(e -> {
             startUpSeveralThreadedModel(Integer.parseInt(numThreadField.getText()));
         });
     }
@@ -73,7 +91,7 @@ public class WebFrame {
     private void initialiseSingleThreadButton() {
         JButton singleThreadFetch = new JButton("Single Thread Fetch");
         panel.add(singleThreadFetch);
-        singleThreadFetch.addActionListener(e->{
+        singleThreadFetch.addActionListener(e -> {
             startUpSingleThreadedModel();
         });
     }
@@ -85,8 +103,8 @@ public class WebFrame {
     private void startUpSeveralThreadedModel(int i) {
         activeThreads = 0;
         finishedThreads = 0;
-        launcher = new Launcher(i);
-        launcher.start();
+        launcherThread = new Launcher(i,defaultTableModel.getRowCount());
+        launcherThread.start();
     }
 
     private void initialiseJFrame() {
@@ -99,49 +117,103 @@ public class WebFrame {
     }
 
     private void initialiseJTable() {
-        defaultTableModel = new DefaultTableModel(new String[]{"url","status" },0);
+        defaultTableModel = new DefaultTableModel(new String[]{"url", "status"}, 0);
         table = new JTable(defaultTableModel);
         table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
         JScrollPane scrollpane = new JScrollPane(table);
-        scrollpane.setPreferredSize(new Dimension(600,300));
+        scrollpane.setPreferredSize(new Dimension(600, 300));
         panel.add(scrollpane);
     }
 
-    public void increaseActiveThreadCount(){
-        synchronized (object){
-            activeThreads+=1;
-            SwingUtilities.invokeLater(()->runningLabel.setText("Running:"+ activeThreads+""));
-        }
+    public void increaseActiveThreadCount() {
+        lock.lock();
+        incrementAndUpdateActiveThread(1);
+        lock.unlock();
     }
 
-    public void decreaseActiveThreadCount(){
-        synchronized (object){
-            activeThreads-=1;
-            SwingUtilities.invokeLater(()->runningLabel.setText("Running:"+ activeThreads+""));
-        }
+    public void decreaseActiveThreadCount() {
+        lock.lock();
+        incrementAndUpdateActiveThread(-1);
+        lock.unlock();
+    }
+
+    private void incrementAndUpdateActiveThread(int delta) {
+        activeThreads += delta;
+        SwingUtilities.invokeLater(() -> labelOfCurrentlyRunning.setText("Running:" + activeThreads + ""));
+    }
+
+    public void activeThreadFinished(int row, String status) {
+        lock.lock();
+        incrementAndUpdateFinishedThreads(row, status);
+        lock.unlock();
+    }
+
+    private void incrementAndUpdateFinishedThreads(int row, String status) {
+        finishedThreads += 1;
+        labelOfAlreadyFinished.setText("Completed: " + finishedThreads + "");
+        progressBar.setValue(finishedThreads);
+        defaultTableModel.setValueAt(status, row, 1);
     }
 
     public class Launcher extends Thread {
         private Semaphore semaphore;
+        private int rows;
 
-        public Launcher(int numWorkers){
+        public Launcher(int numWorkers,int defaultModelRows) {
             semaphore = new Semaphore(numWorkers);
+            this.rows = defaultModelRows;
         }
 
         @Override
-        public void run(){
+        public void run() {
+            try {
+                tryToRun();
+            } catch (InterruptedException e) { }
+        }
+
+        private void tryToRun() throws InterruptedException {
+            runWorkerThreads();
+        }
+
+        private void runWorkerThreads() throws InterruptedException {
             increaseActiveThreadCount();
-
-
-
+            int row = 0;
+            while (row < rows) {
+                useWorkerThreads(row++);
+            }
             decreaseActiveThreadCount();
+        }
+
+        private void useWorkerThreads(int row) throws InterruptedException {
+            WebWorker worker = new WebWorker(defaultTableModel.getValueAt(row, 0).toString(),
+                    row,
+                    WebFrame.this,
+                    ()->tryToAcquire(),
+                    ()->semaphore.release());
+            worker.start();
+        }
+
+        private void tryToAcquire() {
+            try {
+                semaphore.acquire();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(()->{
-            new WebFrame("links.txt");
+        SwingUtilities.invokeLater(() -> {
+            tryToCreateWebFrame();
         });
+    }
+
+    private static void tryToCreateWebFrame() {
+        try {
+            new WebFrame("links.txt");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- WebWorker.java
+++ WebWorker.java
@@ -2,54 +2,86 @@ import java.io.*;
 import java.net.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import javax.swing.*;
 
+//I needed help for this part of homework
 public class WebWorker extends Thread {
-/*
-  This is the core web/download i/o code...
- 	download() {
-		InputStream input = null;
-		StringBuilder contents = null;
-		try {
-			URL url = new URL(urlString);
-			URLConnection connection = url.openConnection();
-		
-			// Set connect() to throw an IOException
-			// if connection does not succeed in this many msecs.
-			connection.setConnectTimeout(5000);
-			
-			connection.connect();
-			input = connection.getInputStream();
+    private String urlStr;
+    private WebFrame webFrame;
+    private int row;
+    private Runnable acquireSemaphore;
+    private Runnable releaseSemaphore;
 
-			BufferedReader reader  = new BufferedReader(new InputStreamReader(input));
-		
-			char[] array = new char[1000];
-			int len;
-			contents = new StringBuilder(1000);
-			while ((len = reader.read(array, 0, array.length)) > 0) {
-				contents.append(array, 0, len);
-				Thread.sleep(100);
-			}
-			
-			// Successful download if we get here
-			
-		}
-		// Otherwise control jumps to a catch...
-		catch(MalformedURLException ignored) {}
-		catch(InterruptedException exception) {
-			// YOUR CODE HERE
-			// deal with interruption
-		}
-		catch(IOException ignored) {}
-		// "finally" clause, to close the input stream
-		// in any case
-		finally {
-			try{
-				if (input != null) input.close();
-			}
-			catch(IOException ignored) {}
-		}
+    public WebWorker(String urlStr, int row, WebFrame webFrame, Runnable acquireSemaphore, Runnable releaseSemaphore){
+        this.row = row;
+        this.urlStr = urlStr;
+        this.webFrame = webFrame;
+        this.releaseSemaphore = releaseSemaphore;
+        this.acquireSemaphore = acquireSemaphore;
+    }
 
-*/
-	
+    @Override
+    public void run(){
+        acquireSemaphore.run();
+        tryToDownload();
+        releaseSemaphore.run();
+    }
+
+    private void tryToDownload() {
+        webFrame.increaseActiveThreadCount();
+        download();
+        webFrame.decreaseActiveThreadCount();
+    }
+
+    private void download(){
+        InputStream input = null;
+        StringBuilder contents = null;
+        try {
+            URL url = new URL(urlStr);
+            URLConnection connection = url.openConnection();
+
+            // Set connect() to throw an IOException
+            // if connection does not succeed in this many msecs.
+            connection.setConnectTimeout(5000);
+
+            connection.connect();
+            input = connection.getInputStream();
+
+            BufferedReader reader  = new BufferedReader(new InputStreamReader(input));
+
+            char[] array = new char[1000];
+            int len;
+            contents = new StringBuilder(1000);
+            while ((len = reader.read(array, 0, array.length)) > 0) {
+                contents.append(array, 0, len);
+                Thread.sleep(100);
+            }
+            notifySuccessfulDownload(contents);
+            // Successful download if we get here
+
+        }
+        // Otherwise control jumps to a catch...
+        catch(MalformedURLException ignored) {}
+        catch(InterruptedException exception) {
+            // YOUR CODE HERE
+            // deal with interruption
+            notifyInterrupt();
+        }
+        catch(IOException ignored) {}
+        // "finally" clause, to close the input stream
+        // in any case
+        finally {
+            try{
+                if (input != null) input.close();
+            }
+            catch(IOException ignored) {}
+        }
+    }
+
+    private void notifyInterrupt() {
+        webFrame.activeThreadFinished(row,"Interrupted");
+    }
+
+    private void notifySuccessfulDownload(StringBuilder contents) {
+        webFrame.activeThreadFinished(row,new SimpleDateFormat("HH:mm:ss").format(new Date()));
+    }
 }
